0ed81853dba071c1032b0eebf0f32c7c615a9d94
jesu-m0 <jesusmd2000@gmail.com>
2026-03-27 18:51:58 +0100

    login and creation page

---
+++ zapcards-spring/src/main/java/com/zapcards/zapcardsspring/ZapCardsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zapcards.zapcardsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapCardsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapCardsSpringApplication.class, args);
+	}
+
+}
